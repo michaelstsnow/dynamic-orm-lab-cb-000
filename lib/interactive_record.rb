763c4fbb8fa121eaff5acc2b6cff6be7f3024f2d
michaelstsnow <michael.st.snow@gmail.com>
2019-07-06 00:55:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,7 @@ class InteractiveRecord
 
   def values_for_insert
     values=[]
-    self.class.column_names.each do |col_name|
+    self.class.column_names.each do |col|
       values << self.send("#{col}").to_s unless send(col_name).nil?
     end
     values.join(", ")
