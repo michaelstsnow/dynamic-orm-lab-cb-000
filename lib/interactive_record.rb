ffbb9ca94aad5147e3dcab91787c1da095c2245c
michaelstsnow <michael.st.snow@gmail.com>
2019-07-06 00:50:21 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ class InteractiveRecord
      column_names=self.col_names_for_insert.split(", ")
      values=[]
      for column_names.each do |col|
-      values << self.send("#{col}").to_s
+      #values << self.send("#{col}").to_s
      end
      #values.join(", ")
   end
